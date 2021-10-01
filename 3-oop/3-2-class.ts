ec10c81d0b083e9d8bbbaa6793d2a7c2200bb6f9
momo <wdudtlrw78@gmail.com>
2021-10-01 21:52:35 +0900

    feat: 객체지향적으로 커피기계 만들기

---
+++ 3-oop/3-2-class.ts
@@ -0,0 +1,38 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    // static을 붙이는 순간 BEANS_GRAMM_PER_SHOT 상수는 오브젝트 레벨이 아니라,
+    // 클래스 레벨로 (클래스 영역의 메모리에 할당이 한번만 됨)
+    // CoffeeMaker 클래스로 만드는 오브젝트마다 변수가 메모리에 할당되지 않고, 클래스에 한번만 저장되어져 있어요.
+
+    static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+}
