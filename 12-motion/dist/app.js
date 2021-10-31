dd12424bddd5fd5009e428cfac6e427a62004f5e
momo <wdudtlrw78@gmail.com>
2021-10-31 18:20:34 +0900

    feat: page component

--- 12-motion/dist/app.js
+++ 12-motion/dist/app.js
@@ -1,2 +1,8 @@
-"use strict";
-console.log('Hello');
+import { PageComponent } from './components/page.js';
+class App {
+    constructor(appRoot) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+    }
+}
+new App(document.querySelector('.document'));
