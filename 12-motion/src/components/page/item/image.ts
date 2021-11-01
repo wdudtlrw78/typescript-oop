32015787b2973554387ac88b96ea9e740914577c
momo <wdudtlrw78@gmail.com>
2021-11-01 20:13:37 +0900

    feat: note, todo, video components

--- 12-motion/src/components/page/item/image.ts
+++ 12-motion/src/components/page/item/image.ts
@@ -5,7 +5,7 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
     super(`
     <section class="image">
         <div class="image__holder"><img class="image__thumbnail"></div>
-        <p class="image__title"></p>
+        <h2 class="image__title"></h2>
     </section>
     `);
 
