400193ee2c209c4ef89cc47a60446d2377ec9adc
momo <wdudtlrw78@gmail.com>
2021-11-26 00:54:30 +0900

    refactor: base component

--- 12-motion/src/components/item/image.ts
+++ 12-motion/src/components/item/image.ts
@@ -1,18 +1,15 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from '../component.js';
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement('template');
-    // 사용자한테 전달받은 data들을 innerHTML에 넣는 것은 위험하다.
-    // 필요한 부분만 업데이트 해주자.
-    template.innerHTML = `
+    super(`
       <section class="image">
         <div class="image__holder">
           <img class="image__thumbnail" />
         </div>
         <p class="image__title"></p>
       </section>
-    `;
-    this.element = template.content.firstElementChild! as HTMLElement;
+    `);
 
     const imageElement = this.element.querySelector(
       '.image__thumbnail'
@@ -24,8 +21,5 @@ export class ImageComponent {
       '.image__title'
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
